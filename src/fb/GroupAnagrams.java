076b0e6a27fbe2a03e935a8bbac9cb6c5e4ae08f
Xiaodong <xiaodz@amazon.com>
2017-11-30 21:52:58 -0800

    Group Anagrams

---
+++ src/fb/GroupAnagrams.java
@@ -0,0 +1,28 @@
+package fb;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class GroupAnagrams {
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> keyToList = new HashMap<>();
+
+        for (String str : strs) {
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+            String sortedStr = new String(chars);
+            if (!keyToList.containsKey(sortedStr)) {
+                keyToList.put(sortedStr, new ArrayList<>());
+            }
+            keyToList.get(sortedStr).add(str);
+        }
+        return  new ArrayList<>(keyToList.values());
+    }
+
+    public static void main(String[] args) {
+        System.out.println(groupAnagrams(new String[]{"eat", "tea", "tan", "ate", "nat", "bat"}));
+    }
+}
