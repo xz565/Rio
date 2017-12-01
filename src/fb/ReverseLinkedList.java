d34a71e268d5a5731331a120be9c71ec6cb0b400
Xiaodong <xiaodz@amazon.com>
2017-11-30 22:06:23 -0800

    Iterative reverse linked list

---
+++ src/fb/ReverseLinkedList.java
@@ -0,0 +1,26 @@
+package fb;
+
+import utils.ListNode;
+
+public class ReverseLinkedList {
+    public ListNode reverseList(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+        ListNode prev = null;
+        ListNode curt = head;
+        ListNode next = head.next;
+
+        while (next != null) {
+            curt.next = prev;
+
+            prev = curt;
+            curt = next;
+            next = next.next;
+        }
+
+        curt.next = prev;
+
+        return curt;
+    }
+}
