e03890dee1ee16d23675115c8b18a07064bdd1df
Xiaodong <xiaodz@amazon.com>
2017-12-30 10:40:00 -0800

    Stage

---
+++ src/common/types/TreeNode.java
@@ -0,0 +1,10 @@
+package common.types;
+
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int x) {
+        val = x;
+    }
+}
---
+++ src/fb/hard/MergeKSortedLists_23.java
@@ -0,0 +1,37 @@
+package fb.hard;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import common.types.ListNode;
+
+public class MergeKSortedLists_23 {
+
+    public ListNode mergeKLists(ListNode[] lists) {
+
+        PriorityQueue<ListNode> pq = new PriorityQueue<>(new Comparator<ListNode>() {
+            @Override
+            public int compare(ListNode o1, ListNode o2) {
+                return o1.val - o2.val;
+            }
+        });
+
+        for (ListNode node : lists) {
+            if (node != null) {
+                pq.add(node);
+            }
+        }
+
+        ListNode head = new ListNode(0);
+        ListNode curt = head;
+
+        while(!pq.isEmpty()) {
+            ListNode node = pq.poll();
+            curt.next = node;
+            curt = curt.next;
+            if (node.next != null) {
+                pq.add(node.next);
+            }
+        }
+        return head.next;
+    }
+}
